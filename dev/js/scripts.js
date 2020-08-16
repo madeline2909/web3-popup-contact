2f8096cb063167767c8377fb137fa8bf8c1c3a3c
madeline2909 <63311619+madeline2909@users.noreply.github.com>
2020-08-16 01:10:57 -0400

    overlaying form over music box when clicked

--- dev/js/scripts.js
+++ dev/js/scripts.js
@@ -1,10 +1,9 @@
 
 // @codekit-prepend "cd-animation.js";
-// @codekit-prepend "X-animation.js";
-
 // @codekit-prepend "music-box-sound.js";
 // @codekit-prepend "music-box-animation.js";
 // @codekit-prepend "music-box-events.js";
+// @codekit-prepend "X-animation.js";
 
 
 // $( document ).ready(function() {
