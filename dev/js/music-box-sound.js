2c2e08d72b1fa87564f374cd0baef330136d71d7
qiujin123 <qiujinzou123@gmail.com>
2020-08-16 03:21:47 -0400

    fixed sound #feels good

--- dev/js/music-box-sound.js
+++ dev/js/music-box-sound.js
@@ -1,5 +1,24 @@
-function play(){
-    var audio = document.getElementById("audio");
-    audio.play();
-                  }
+
+
+// $("#music").on("mouseenter", function(){
+//     audio.currentTime = 0;
+//     audio.play();
+// });
+// $("#music").on("mouseleave", function(){
+//     audio.currentTime = 0;
+//     audio.play();
+// }
+
+
+
+function PlaySound(){
+    var thissound=document.getElementById("audio");
+    thissound.play();
+}
+
+function StopSound(){
+    var thissound=document.getElementById("audio");
+    thissound.pause();
+    thissound.currentTime = 0;
+}
     
